027f4aad78de7ea2bc57622465196943b24b02d6
mel3ven <clausenmelf@gmail.com>
2023-05-15 16:20:53 +0200

    use actual ec2 ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c94855ba95c574c8"
+  ami           = "ami-0ec7f9846da6b0f61"
   instance_type = "t2.micro"
 
   key_name = "opencampus-test"
